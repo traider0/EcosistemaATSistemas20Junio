7ca09f2cc44363bbb8b9a92cca5601d37b1f78db
Victor <victorherrerocazurro@gmail.com>
2016-06-21 20:03:19 +0200

    Plugins: install, scm, war, release, surefire, failsafe, ....

---
+++ 08-PluginSurefire/src/main/java/com/curso/ecosistema/Calculadora.java
@@ -0,0 +1,11 @@
+package com.curso.ecosistema;
+
+public class Calculadora {
+	public int suma(int a, int b) {
+		return a + b;
+	}
+	
+	public int resta(int a, int b) {
+		return a - b;
+	}
+}
---
+++ 08-PluginSurefire/src/test/java/com/curso/ecosistema/TestCalculadoraIT.java
@@ -0,0 +1,16 @@
+package com.curso.ecosistema;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TestCalculadoraIT {
+
+	@Test
+	public void test() {
+		//Assert.fail();
+	}
+
+}
